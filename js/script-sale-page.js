6d95521e4683f600a29f83d9f9be652490e09ba2
MatveyKa <govrinisranbik@gmail.com>
2025-08-13 10:36:39 +0300

    last page

---
+++ js/script-sale-page.js
@@ -0,0 +1,36 @@
+const serachVars = document.querySelectorAll('.right-top__var')
+const serachFilterVars = document.querySelectorAll('.right-top__right-var')
+
+serachVars.forEach((el) => {
+   el.onclick = () => {
+      for (let i = 0; i < serachVars.length; i++) {
+         serachVars[i].classList.remove('right-top__var--active')
+      }
+      el.classList.add('right-top__var--active')
+   }
+})
+serachFilterVars.forEach((el) => {
+   el.onclick = () => {
+      for (let i = 0; i < serachFilterVars.length; i++) {
+         serachFilterVars[i].classList.remove('right-top__right-var--active')
+      }
+      el.classList.add('right-top__right-var--active')
+      if (el == serachFilterVars[1]) {
+         document.querySelector('.right-main__grid').classList.add('right-main__grid--lines')
+      } else document.querySelector('.right-main__grid').classList.remove('right-main__grid--lines')
+   }
+})
+const gridVar = () => {
+   const serachFilterVars = document.querySelectorAll('.right-top__right-var')
+   const mainGrid = document.querySelector('.right-main__grid')
+   if (window.innerWidth <= 600) {
+      serachFilterVars.forEach((el) => {
+         el.classList.remove('right-top__right-var--active')
+      })
+      serachFilterVars[0].classList.add('right-top__right-var--active')
+      mainGrid.classList.remove('right-main__grid--lines')
+   }
+}
+window.addEventListener('resize', () => {
+   gridVar();
+});
